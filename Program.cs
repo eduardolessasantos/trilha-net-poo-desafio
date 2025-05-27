3cc9b814fbe94483efd4ff7f9310bbd94974d3be
Eduardo Lessa <eduardolessa2011@gmail.com>
2025-05-27 14:53:12 -0300

    Entrega desafio eduardolessa

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone i = new Iphone("333333333", "IX30", "2345", 86);
+i.Ligar();
+i.InstalarAplicativo("Telegram");
+
+Console.WriteLine("-------------------------");
+
+Nokia n = new Nokia("444444444", "GALAXY", "5647", 96);
+n.ReceberLigacao();
+n.InstalarAplicativo("Waze");
